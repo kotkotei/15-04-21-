5619b46c23cbb51cc5d27465d48e7729f8eae65b
kotkotei <kotkotei@yandex.ru>
2021-04-15 14:00:36 +0700

    vrjvhohtohjty8ohwhthoyhrh

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ввод число ");
+            int d = Convert.ToInt32(Console.ReadLine());
+            int a = d % 10;
+            int b = d / 100;
+            int s = d / 10 % 10;
+            if ((a == b) & (b == s) & (s == a))
+            {
+                Console.WriteLine("raven bich AAAAAA!!!!!!");
+            }
+            else
+            {
+                Console.WriteLine("no raven aaa!!!!1");
+                if ((a == b) | (b == s) | (s == a))
+                { Console.WriteLine("da"); }
+                { Console.WriteLine("no"); }
+            }
+            Console.ReadKey();
+        }
+    }
+}
